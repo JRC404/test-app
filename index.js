56d5b58360eb6f9d80cdb9f5cd8dd3811b405713
Jacob Reilly-Cooper <jrcooper@firebrandtraining.com>
2023-09-25 15:17:11 +0100

    Challenge

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("ello");
 console.log("Hi, Andrea");
 console.log("Hi, Talal");
+
+// Create a new branch
+// checkout that branch
+// add to the branch, commit and push from the branch
+// checkout to main
+// complete a pull request
+// delete the old branch
+// relax
